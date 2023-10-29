dd4228a0dd542149c8ec527b8d1e28d41eb09cd8
arashsafari <arash.safari101@gmail.com>
2023-10-29 11:55:46 +0330

    feat:create rate model

---
+++ database/factories/RateFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Currency;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rate>
+ */
+class RateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'currency_key' => Currency::factory(),
+            'rate' => fake()->randomDigit(),
+            'rate_currency_key' => Currency::factory(),
+        ];
+    }
+}
---
+++ database/migrations/2023_10_29_075417_create_rates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency_key');
+            $table->foreign('currency_key')->references('key')->on('currencies')->onDelete('cascade')->onUpdate('cascade');
+            $table->double('rate');
+            $table->string('rate_currency_key');
+            $table->foreign('rate_currency_key')->references('key')->on('currencies')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rates');
+    }
+};
